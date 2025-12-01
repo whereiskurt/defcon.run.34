abf3e43b4b6dd9d2a05250d1aa9ad8e3dbb24168
KPH <whereiskurt@gmail.com>
2025-11-30 19:55:48 -0500

    -Fixed chunk mismatch between image and bucket

--- apps/auth/deploy.webapp.sh
+++ apps/auth/deploy.webapp.sh
@@ -13,15 +13,27 @@ export REGION_SHORT=${REGION_SHORT:-"use1"}
 export WEBAPP_ORIGIN_BUCKET=$(aws ssm get-parameter --name "/dc34/cloudfront-assets/${REGION_SHORT}/auth/bucket_name" --region "${AWS_REGION}" --query "Parameter.Value" --output text)
 
 
-cd webapp
-export NEXT_PUBLIC_ASSET_PREFIX=${WEBAPP_PREFIX}/public
-npm run build
-AWS_PROFILE=application aws s3 sync .next/static s3://${WEBAPP_ORIGIN_BUCKET}/${WEBAPP_PREFIX}/_next/static --cache-control 'public,max-age=31536000,immutable' --delete --exclude '*.map'
-AWS_PROFILE=application aws s3 cp .next/server/app/index.html s3://${WEBAPP_ORIGIN_BUCKET}/index.html --cache-control 'public,max-age=31536000,immutable'
-AWS_PROFILE=application aws s3 sync public s3://${WEBAPP_ORIGIN_BUCKET}/${WEBAPP_PREFIX}/public --cache-control 'public,max-age=31536000,immutable' --delete
-cd ..
+export NEXT_PUBLIC_ASSET_PREFIX="/${WEBAPP_PREFIX}/public"
 
-docker buildx build --platform=linux/amd64 -t $REPO_NAME:$IMAGE_TAG -f webapp/Dockerfile.webapp ./webapp/
+# Build Docker image (amd64 for ECS)
+docker buildx build --platform=linux/amd64 \
+  --build-arg NEXT_PUBLIC_ASSET_PREFIX="${NEXT_PUBLIC_ASSET_PREFIX}" \
+  --build-arg WEBAPP_PREFIX="${WEBAPP_PREFIX}" \
+  --build-arg WEBAPP_ORIGIN="${WEBAPP_ORIGIN}" \
+  -t $REPO_NAME:$IMAGE_TAG -f webapp/Dockerfile.webapp ./webapp/
+
+# Extract static assets from Docker image and sync to S3
+CONTAINER_ID=$(docker create $REPO_NAME:$IMAGE_TAG)
+rm -rf /tmp/next-static /tmp/next-public
+docker cp $CONTAINER_ID:/app/.next/static /tmp/next-static
+docker cp $CONTAINER_ID:/app/.next/server/app/index.html /tmp/next-static
+docker cp $CONTAINER_ID:/app/public /tmp/next-public
+docker rm $CONTAINER_ID
+
+AWS_PROFILE=application aws s3 sync /tmp/next-static s3://${WEBAPP_ORIGIN_BUCKET}/${WEBAPP_PREFIX}/_next/static --cache-control 'public,max-age=31536000,immutable' --delete --exclude '*.map'
+AWS_PROFILE=application aws s3 cp /tmp/next-static/index.html s3://${WEBAPP_ORIGIN_BUCKET}/index.html --cache-control 'public,max-age=31536000,immutable'
+AWS_PROFILE=application aws s3 sync /tmp/next-public s3://${WEBAPP_ORIGIN_BUCKET}/${WEBAPP_PREFIX}/public --cache-control 'public,max-age=31536000,immutable' --delete
+
 
 aws ecr get-login-password --region ${AWS_REGION} \
   | docker login --username AWS --password-stdin "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
